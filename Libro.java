cf2c73327a066a2eaa79e2c48b2d6a091781a8b1
Gustavo Hernandez <162768131+Guzmonkey@users.noreply.github.com>
2024-05-20 22:03:02 -0700

    Update Libro.java

--- Libro.java
+++ Libro.java
@@ -2,33 +2,39 @@ public class Libro extends ElementoBiblioteca {
     /*
      * Declaracion de variables
      */
-    private int numPaginas;
-    private int numCapitulos;
+    private String numPaginas;
+    private String numCapitulos;
     /*
      * Constructor de la clase
      */
-    public Libro(String titulo, int anioPublicacion, int numIdentificacion, String genero, String autor, int numPaginas, int numCapitulos) {
+    public Libro(String titulo, String anioPublicacion, String numIdentificacion, String genero, String autor, String numPaginas, String numCapitulos) {
         super(titulo, anioPublicacion, numIdentificacion, genero, autor);
         this.numPaginas = numPaginas;
         this.numCapitulos = numCapitulos;
     }
+
     /*
      * Getters y setters 
      */
-    public int getNumPaginas() {
+    public String getNumPaginas() {
         return numPaginas;
     }
 
-    public void setNumPaginas(int numPaginas) {
+    public void setNumPaginas(String numPaginas) {
         this.numPaginas = numPaginas;
     }
 
-    public int getNumCapitulos() {
+    public String getNumCapitulos() {
         return numCapitulos;
     }
 
-    public void setNumCapitulos(int numCapitulos) {
+    public void setNumCapitulos(String numCapitulos) {
         this.numCapitulos = numCapitulos;
-    } 
+    }
+
+    @Override
+    public void mostrarInformacion(){
+        System.out.println("Numero de paginas: " + numPaginas + "\nNumero de capitulos: " + numCapitulos);
+    }
 
 }
