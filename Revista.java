5088005090a843be7a11366385162674b8d19ee1
Gustavo Hernandez <162768131+Guzmonkey@users.noreply.github.com>
2024-05-20 22:03:15 -0700

    Update Revista.java

--- Revista.java
+++ Revista.java
@@ -1,24 +1,35 @@
 public class Revista extends ElementoBiblioteca{
-    private int numEjemplares;
+    private String numEjemplares;
     private String editores;
-    public Revista(String titulo, int anioPublicacion, int numIdentificacion, String genero, String autor, int numEjemplares, String editores) {
+    
+    public Revista(String titulo, String anioPublicacion, String numIdentificacion, String genero, String autor, String numEjemplares, String editores) {
         super(titulo, anioPublicacion, numIdentificacion, genero, autor);
         this.numEjemplares = numEjemplares;
         this.editores = editores;
     }
-    
-    public int getNumEjemplares() {
+
+    public String getNumEjemplares() {
         return numEjemplares;
     }
-    public void setNumEjemplares(int numEjemplares) {
+
+
+    public void setNumEjemplares(String numEjemplares) {
         this.numEjemplares = numEjemplares;
     }
+
+
     public String getEditores() {
         return editores;
     }
+
+
     public void setEditores(String editores) {
         this.editores = editores;
     }
 
-    
+     @Override
+    public void mostrarInformacion(){
+        System.out.println("Numero de ejemplares: " + numEjemplares + "\nEditores: " + editores);
+    }
+
 }
